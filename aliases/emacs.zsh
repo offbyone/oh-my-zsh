1f1ace1cc4cb7048be4a245105d2376876761339
Chris Rose <offby1@offby1.net>
2012-01-25 10:54:23 -0800

    Added a root emacs alias

--- aliases/emacs.zsh
+++ aliases/emacs.zsh
@@ -3,8 +3,10 @@ if [[ $EMACS = t ]]; then
     export VISUAL=emacsclient
     export PAGER=cat
     alias emacs='emacsclient'
+    alias E="SUDO_EDITOR=\"emacsclient -c -a emacs\" sudoedit"
 else
     export EDITOR=${EDITOR:-emacs}
     export VISUAL=${VISUAL:-emacs}
     export PAGER=${PAGER:-less}
+    alias E=sudoedit
 fi
