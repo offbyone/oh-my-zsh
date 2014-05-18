ca6d95cd9aaa5cb4e9aaf74d76f331bfe5b80f7a
Chris Rose <offby1@offby1.net>
2014-05-18 07:39:26 -0700

    [pythonbrew] add the brew dir to the path

--- plugins/pythonbrew/pythonbrew.plugin.zsh
+++ plugins/pythonbrew/pythonbrew.plugin.zsh
@@ -1,5 +1,12 @@
 function ensure_pythonbrew() {
+    if [[ -d $HOME/.pythonz ]]; then
+        __python_brew_dir=$HOME/.pythonz
+        [[ -x $HOME/.pythonz/bin/pythonz ]] && return 0
+        return 1
+    fi
+
     if [[ -d $HOME/.pythonbrew ]]; then
+        __python_brew_dir=$HOME/.pythonbrew
         [[ -x $HOME/.pythonbrew/bin/pythonbrew ]] && return 0
         return 1
     fi
@@ -9,6 +16,7 @@ function ensure_pythonbrew() {
 }
 
 if ensure_pythonbrew; then
-    append_path PATH $HOME/.pythonbrew/bin
+    append_path PATH $__python_brew_dir/bin
+    unset __python_brew_dir
     export PATH
 fi
