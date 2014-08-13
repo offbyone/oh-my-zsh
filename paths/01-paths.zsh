b199189c1bf07b0ab4fe1647a292e602518b098f
Chris Rose <offby1@offby1.net>
2014-08-12 22:41:31 -0700

    Added $HOME/.local to paths.

--- paths/01-paths.zsh
+++ paths/01-paths.zsh
@@ -22,6 +22,7 @@ done
 
 # : use my binaries first
 prepend_path PATH $HOME/local/bin
+prepend_path PATH $HOME/.local/bin
 prepend_path PATH $HOME/bin
 prepend_path PATH $HOME/scripts
 
@@ -30,6 +31,7 @@ prepend_path MANPATH /usr/share/man
 prepend_path MANPATH /usr/local/share/man
 prepend_path MANPATH /usr/local/man
 prepend_path MANPATH $HOME/local/man
+prepend_path MANPATH $HOME/.local/man
 
 # add $HOME/local to PKG_CONFIG_PATH
 prepend_path PKG_CONFIG_PATH $HOME/local/lib/pkgconfig
