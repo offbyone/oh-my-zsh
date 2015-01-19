58e77dc1ae2c4bc54c8f5eb7933d7a8b00df7bdb
Chris Rose <chrisros@amazon.com>
2015-01-19 09:45:11 -0800

    Handle local filesystem ident when the mount path is long

--- lib/mount.zsh
+++ lib/mount.zsh
@@ -1,7 +1,10 @@
 function is_local_fs() {
     local dir="${1:-$(pwd)}";
 
-    local local_root=$(df -l "$dir" | awk '/^Filesystem/ {next}; {print $6}')
+    local local_root=$(df -l "$dir" | awk 'BEGIN {offset=6};
+/^Filesystem/ {next};
+NF == 1 {offset=5; next};
+{print $offset}')
 
     if [[ -n $local_root ]]; then
         return 0
