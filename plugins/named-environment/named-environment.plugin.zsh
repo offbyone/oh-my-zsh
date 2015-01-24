5788fb0ce8724795b9814154d6c1a7a932aaf252
Chris Rose <offby1@offby1.net>
2015-01-23 21:05:12 -0800

    is_local_fs moved to named-environment

--- plugins/named-environment/named-environment.plugin.zsh
+++ plugins/named-environment/named-environment.plugin.zsh
@@ -49,3 +49,19 @@ function get_env_name () {
 }
 
 set_environment `get_env_name`
+
+
+function is_local_fs() {
+    local dir="${1:-$(pwd)}";
+
+    local local_root=$(df -l "$dir" | awk 'BEGIN {offset=6};
+/^Filesystem/ {next};
+NF == 1 {offset=5; next};
+{print $offset}')
+
+    if [[ -n $local_root ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
